c08e07cc3d24de8f672fbf5a49bd2cc19ae2bd18
sauravgupta2800 <sauravgupta2800@gmail.com>
2021-06-05 17:50:03 +0530

    dynamic rendered component

---
+++ src/Components/UI/DraggableGrid/Test1.js
@@ -0,0 +1,3 @@
+export const Test1 = () => <div>test 1</div>;
+export const Test2 = () => <div>test 2</div>;
+export const Test3 = () => <div>test 3</div>;
---
+++ src/Components/UI/DraggableGrid/config.js
@@ -0,0 +1,19 @@
+import { Test1, Test2, Test3 } from "./Test1";
+
+export const COMPONENTS = [
+  {
+    component: Test1,
+    key: "a",
+    dataGrid: { x: 0, y: 0, w: 2, h: 5, minW: 1, minH: 5 },
+  },
+  {
+    component: Test2,
+    key: "b",
+    dataGrid: { x: 2, y: 0, w: 2, h: 5, minW: 1, minH: 5 },
+  },
+  {
+    component: Test3,
+    key: "c",
+    dataGrid: { x: 4, y: 0, w: 2, h: 5, minW: 1, minH: 5 },
+  },
+];
