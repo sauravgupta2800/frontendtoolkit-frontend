19b2fce00b8a5f9460b4cc5ba97eb508b598c0c6
sauravgupta2800 <sauravgupta2800@gmail.com>
2021-06-06 04:49:54 +0530

    ant design installed

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom";
 import "../node_modules/react-grid-layout/css/styles.css";
 import "../node_modules/react-resizable/css/styles.css";
 
+import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
 import "bootstrap/dist/css/bootstrap.css";
 import "./assets/styles/main.scss";
 
