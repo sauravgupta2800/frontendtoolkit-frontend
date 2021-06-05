e65d1c6b2bc92c61476ec0b29b5ea194857a4de8
sauravgupta2800 <sauravgupta2800@gmail.com>
2021-06-06 04:52:14 +0530

    autocomplete added

--- src/Components/UI/Common/DragCard/DragCard.js
+++ src/Components/UI/Common/DragCard/DragCard.js
@@ -1,6 +1,13 @@
 import Icon from "../Icon/Icon";
+import React from "react";
 
-const DragCard = ({ id = "npm", title = "Package Details" }) => {
+const DragCard = ({
+  id = "npm",
+  title = "Card Details",
+  onInfoClick,
+  onDeleteClick,
+  ...rest
+}) => {
   return (
     <div className="w-100 h-100 border rounded-3 d-flex flex-column ft-bg-light100 p-4">
       <div className="d-flex justify-content-between">
@@ -15,20 +22,35 @@ const DragCard = ({ id = "npm", title = "Package Details" }) => {
           <div className="fs-2 fw-bold ms-3 ft-color-dark">{title}</div>
         </div>
         <div className="d-flex justify-content-between align-items-center ft-card-action-icon">
-          <Icon id="info" size={"md"} />
-          <Icon id="delete" size={"md"} />
+          <Icon
+            id="info"
+            size={"md"}
+            title={"View Info"}
+            onClick={onInfoClick}
+          />
+          <Icon
+            id="delete"
+            size={"md"}
+            title={"Remove Card"}
+            onClick={onDeleteClick}
+          />
           <div className="ms-2">
             <Icon
               id="drag"
               size={"sm"}
               withWrapper={false}
+              showCursor={false}
               iconClass="ft-color-light1"
             />
           </div>
         </div>
       </div>
-      <div>B</div>
-      <div>C</div>
+      <div className="w-100 d-flex justify-content-center">
+        {rest.contentSlot}
+      </div>
+      <div className="w-100 d-flex justify-content-center">
+        {rest.actionSlot}
+      </div>
     </div>
   );
 };
--- src/Components/UI/PackageDetails/PackageCard.js
+++ src/Components/UI/PackageDetails/PackageCard.js
@@ -1,7 +1,79 @@
+import React, { useState } from "react";
+import { AutoComplete } from "antd";
 import DragCard from "../Common/DragCard/DragCard";
+import axios from "axios";
 
 const PackageCard = () => {
-  return <DragCard class="w-100 h-100"></DragCard>;
+  // const [value, setValue] = useState("");
+  const [options, setOptions] = useState([]);
+
+  const infoHandler = () => {
+    console.log("info handler");
+  };
+  const deleteHandler = () => {
+    console.log("delete handler");
+  };
+  const renderItem = ({ name, description }) => {
+    return {
+      value: name,
+      label: (
+        <div className="d-flex flex-column">
+          <div className="fw-bold ft-color-dark1">{name}</div>
+          <div className={"ft-color-$dark2"}>{description}</div>
+        </div>
+      ),
+    };
+  };
+  const onSearch = async (q = "") => {
+    console.log("q: ", q);
+    if (!q) {
+      setOptions([]);
+      return;
+    }
+    const params = {
+      q,
+    };
+    try {
+      const { data } = await axios.get("/api/packages", { params });
+      const list = data.data.map((item) => renderItem(item.package));
+      setOptions(list);
+    } catch {
+      //
+    }
+  };
+  const onSelect = (data) => {
+    console.log("onSelect", data);
+  };
+
+  const contentSlot = () => {
+    return (
+      <div className="w-100">
+        <div class="mt-3 ft-color-dark2 fs-4">
+          find the cost of adding a npm package to your bundle
+        </div>
+        <AutoComplete
+          options={options}
+          className="w-100 mt-4"
+          allowClear={true}
+          defaultActiveFirstOption={true}
+          placeholder="find package"
+          dropdownClassName="packages-autocomplete-dropdown"
+          onSelect={onSelect}
+          onSearch={onSearch}
+        />
+      </div>
+    );
+  };
+
+  return (
+    <DragCard
+      class="w-100 h-100"
+      title={"Package Details"}
+      onInfoClick={infoHandler}
+      onDeleteClick={deleteHandler}
+      contentSlot={contentSlot()}
+    />
+  );
 };
 
 export default PackageCard;
