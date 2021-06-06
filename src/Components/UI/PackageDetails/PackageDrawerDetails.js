5237ba5e602cf8f2ab7e913e165a116282b8f27f
sauravgupta2800 <sauravgupta2800@gmail.com>
2021-06-07 00:04:55 +0530

    conditional check added

--- src/Components/UI/PackageDetails/PackageDrawerDetails.js
+++ src/Components/UI/PackageDetails/PackageDrawerDetails.js
@@ -3,18 +3,38 @@ import PackageSearch from "./PackageSearch";
 import axios from "axios";
 import { PACKAGE } from "../../../shared/endpoints";
 import { bytesToSize } from "./../../utils";
+import isEmpty from "lodash/isEmpty";
+import VersionHistoryChart from "./VersionHistoryChart";
+
 const PackageDrawerDetails = ({ drawerExtraDetails = {} }) => {
-  const [state, setState] = useState({ sizeDetails: {} });
+  const [state, setState] = useState({
+    sizeDetails: {},
+    fetchingSize: true,
+    packageName: "",
+  });
 
   const onOptionSelect = (packageName) => {
     getPackageSize(packageName);
   };
 
   const getPackageSize = async (packageName) => {
-    const { data } = await axios.get(PACKAGE.PACKAGE_SIZE, {
-      params: { package: packageName },
+    setState((prevState) => {
+      return { ...prevState, ...{ fetchingSize: true, packageName } };
     });
-    setState({ sizeDetails: data.data });
+    try {
+      const { data } = await axios.get(PACKAGE.PACKAGE_SIZE, {
+        params: { package: packageName },
+      });
+      setState((prevState) => {
+        return { ...prevState, ...{ sizeDetails: data.data } };
+      });
+    } catch {
+      //
+    } finally {
+      setState((prevState) => {
+        return { ...prevState, ...{ fetchingSize: false } };
+      });
+    }
   };
 
   useEffect(() => {
@@ -32,42 +52,61 @@ const PackageDrawerDetails = ({ drawerExtraDetails = {} }) => {
         </div>
       </div>
       {/* Size row */}
-      <div className="d-flex flex-column justify-content-center align-items-center my-5">
-        <div className="text-uppercase fs-1 ft-color-dark3 mb-4">
-          bundle size
+      {!isEmpty(state.sizeDetails) ? (
+        <div className="d-flex flex-column justify-content-center align-items-center my-5">
+          <div className="text-uppercase fs-1 ft-color-dark3 mb-4">
+            bundle size
+          </div>
+          <div className="d-flex">
+            {state.sizeDetails.size && (
+              <div className="d-flex flex-column justify-content-center align-items-center border px-5 py-3 border-1 rounded-6 me-5">
+                <div className="d-flex justify-content-center align-items-end">
+                  <div className="ft-fs-1 fw-bold">
+                    {bytesToSize(state.sizeDetails.size).value}
+                  </div>
+                  <div className="ft-fs-2 fw-bold ft-color-dark2 ms-2">
+                    {bytesToSize(state.sizeDetails.size).label}
+                  </div>
+                </div>
+                <div className="ft-color-dark2 text-uppercase">minified</div>
+              </div>
+            )}
+            {state.sizeDetails.gzip && (
+              <div className="d-flex flex-column justify-content-center align-items-center border px-5 py-3 border-1 rounded-6 me-5">
+                <div className="d-flex justify-content-center align-items-end">
+                  <div className="ft-fs-1 fw-bold">
+                    {bytesToSize(state.sizeDetails.gzip).value}
+                  </div>
+                  <div className="ft-fs-2 fw-bold ft-color-dark2 ms-2">
+                    {bytesToSize(state.sizeDetails.gzip).label}
+                  </div>
+                </div>
+                <div className="ft-color-dark2 text-uppercase">
+                  minified + Gzipped
+                </div>
+              </div>
+            )}
+            {state.sizeDetails.version && (
+              <div className="d-flex flex-column justify-content-center align-items-center border px-5 py-3 border-1 rounded-6">
+                <div className="d-flex justify-content-center align-items-end">
+                  <div className="ft-fs-1 fw-bold">
+                    {state.sizeDetails.version}
+                  </div>
+                </div>
+                <div className="ft-color-dark2 text-uppercase">version</div>
+              </div>
+            )}
+          </div>
         </div>
-        <div className="d-flex">
-          <div className="d-flex flex-column justify-content-center align-items-center border px-5 py-3 border-1 rounded-6 me-5">
-            <div className="d-flex justify-content-center align-items-end">
-              <div className="ft-fs-1 fw-bold">
-                {bytesToSize(state.sizeDetails.size).value}
-              </div>
-              <div className="ft-fs-2 fw-bold ft-color-dark2 ms-2">
-                {bytesToSize(state.sizeDetails.size).label}
-              </div>
-            </div>
-            <div className="ft-color-dark2 text-uppercase">minified</div>
-          </div>
-          <div className="d-flex flex-column justify-content-center align-items-center border px-5 py-3 border-1 rounded-6 me-5">
-            <div className="d-flex justify-content-center align-items-end">
-              <div className="ft-fs-1 fw-bold">
-                {bytesToSize(state.sizeDetails.gzip).value}
-              </div>
-              <div className="ft-fs-2 fw-bold ft-color-dark2 ms-2">
-                {bytesToSize(state.sizeDetails.gzip).label}
-              </div>
-            </div>
-            <div className="ft-color-dark2 text-uppercase">
-              minified + Gzipped
-            </div>
-          </div>
-          <div class="d-flex flex-column justify-content-center align-items-center border px-5 py-3 border-1 rounded-6">
-            <div class="d-flex justify-content-center align-items-end">
-              <div className="ft-fs-1 fw-bold">{state.sizeDetails.version}</div>
-            </div>
-            <div className="ft-color-dark2 text-uppercase">version</div>
-          </div>
-        </div>
+      ) : (
+        state.fetchingSize && <div>Loading state</div>
+      )}
+
+      {/* Chart */}
+      <div>
+        {state.packageName && (
+          <VersionHistoryChart packageName={state.packageName} />
+        )}
       </div>
     </div>
   );
