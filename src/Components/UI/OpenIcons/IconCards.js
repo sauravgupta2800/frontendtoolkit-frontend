4b391ef89e1789ba98b71beaa2fe77d279c03ad0
sauravgupta2800 <sauravgupta2800@gmail.com>
2021-06-13 19:18:02 +0530

    copy to clipboard added for icons

--- src/Components/UI/OpenIcons/IconCards.js
+++ src/Components/UI/OpenIcons/IconCards.js
@@ -1,3 +1,5 @@
+import { useClipboard } from "use-clipboard-copy";
+
 const IconCards = ({ list }) => {
   return (
     <div className="d-flex flex-wrap">
@@ -9,6 +11,9 @@ const IconCards = ({ list }) => {
 };
 
 const IconCard = ({ item }) => {
+  const clipboardSvg = useClipboard({ copiedTimeout: 750 });
+  const clipboardCSS = useClipboard({ copiedTimeout: 750 });
+
   return (
     <div className="ft-icon-card w-fit h-fit m-3">
       <div className="icon-wrap ft-bg-prime98 ft-style-1-shadow-hover position-relative">
@@ -20,11 +25,17 @@ const IconCard = ({ item }) => {
           />
         </div>
         <div className="btns-wrap position-absolute w-100 h-100 top-0 start-0">
-          <div className="btn btn1 h-50 w-100 d-flex align-items-center justify-content-center">
-            {`Copy SVG`}
+          <div
+            className="fs-6 btn btn1 h-50 w-100 d-flex align-items-center justify-content-center"
+            onClick={() => clipboardSvg.copy(item.svg)}
+          >
+            {`${clipboardSvg.copied ? "Copied" : "Copy"} SVG`}
           </div>
-          <div className="btn btn2 h-50 w-100 d-flex align-items-center justify-content-center">
-            {`Copy CSS`}
+          <div
+            className="fs-6 btn btn2 h-50 w-100 d-flex align-items-center justify-content-center"
+            onClick={() => clipboardCSS.copy(item.css)}
+          >
+            {`${clipboardCSS.copied ? "Copied" : "Copy"} CSS`}
           </div>
         </div>
       </div>
