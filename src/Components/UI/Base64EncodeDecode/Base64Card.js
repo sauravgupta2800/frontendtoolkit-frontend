cc5e8851b46ab2da6d974aa916810a7d81343d3f
sauravgupta2800 <sauravgupta2800@gmail.com>
2021-06-19 05:06:55 +0530

    base64card done

--- src/Components/UI/Base64EncodeDecode/Base64Card.js
+++ src/Components/UI/Base64EncodeDecode/Base64Card.js
@@ -1,25 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import DragCard from "../Common/DragCard/DragCard";
 import { useDispatch } from "react-redux";
 import { setDrawerData } from "../../../store/cardSlice";
 import { useHistory } from "react-router-dom";
-import { Button } from "antd";
+import { Radio } from "antd";
+import { TYPES } from "./config";
 
 const Base64Card = ({ ...rest }) => {
   const dispatch = useDispatch();
   const history = useHistory();
+  const [value, setValue] = useState("encode");
 
-  const onDetailsSelect = () => {
-    dispatch(setDrawerData({}));
+  const onButtonClick = (key) => {
+    setValue(key);
+    dispatch(setDrawerData({ type: key }));
     history.replace(`/cards/${rest.key_name}`);
   };
 
   const contentSlot = () => {
     return (
       <div className="w-100 d-flex justify-content-center">
-        <Button onClick={onDetailsSelect} className="my-4" type="primary">
-          Encode/Decode
-        </Button>
+        <div className="d-flex justify-content-center w-100">
+          <Radio.Group value={value} size="medium" buttonStyle="solid">
+            {TYPES.map((item) => (
+              <Radio.Button
+                key={item.key}
+                value={item.key}
+                onClick={() => onButtonClick(item.key)}
+              >
+                {item.title}
+              </Radio.Button>
+            ))}
+          </Radio.Group>
+        </div>
       </div>
     );
   };
