1d1704b669e4859b1c7831f61b4681e896af26bb
sauravgupta2800 <sauravgupta2800@gmail.com>
2021-06-23 18:11:21 +0530

    comment removed

--- src/Components/UI/SVGConversion/SVGConversionDetails.js
+++ src/Components/UI/SVGConversion/SVGConversionDetails.js
@@ -2,33 +2,6 @@ import { useState } from "react";
 import SVGUploadOrPaste from "./SVGUploadOrPaste";
 import SVGConversionLayout from "./SVGConversionLayout";
 
-// import svgr from "@svgr/core";
-
-/*
-
-    const svgCode = `
-<svg xmlns="http://www.w3.org/2000/svg"
-  xmlns:xlink="http://www.w3.org/1999/xlink">
-  <rect x="10" y="10" height="100" width="100"
-    style="stroke:#ff0000; fill: #0000ff"/>
-</svg>
-`;
-    svgr(svgCode, { icon: true }, { componentName: "MyComponent" }).then(
-      (jsCode) => {
-        console.log(jsCode);
-      }
-    );
-
-    svgr(
-      svgCode,
-      { icon: true, native: true },
-      { componentName: "MyComponent" }
-    ).then((jsCode) => {
-      console.log(jsCode);
-    })
-
-*/
-
 const SVGConversionDetails = () => {
   const [state, setstate] = useState({ showUpload: false });
 
