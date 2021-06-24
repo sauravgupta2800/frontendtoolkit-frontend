bf5da8869ae2d387355a92606854d90c70493ca0
sauravgupta2800 <sauravgupta2800@gmail.com>
2021-06-25 00:51:51 +0530

    seacrh bar added

--- src/Components/Layout/Sidebar.js
+++ src/Components/Layout/Sidebar.js
@@ -6,19 +6,16 @@ const Sidebar = () => {
     <div className="ft-sidebar border w-100 h-100 d-flex flex-column">
       <div className="flex-grow-1">
         <div className="d-flex h-100 flex-column justify-content-center align-items-center">
-          <SideBarIcon id="home" tooltipText="Home" />
+          <SideBarIcon id="home" tooltipText="Home" selected={true} />
           <SideBarIcon id="add" tooltipText="Add" />
           <SideBarIcon id="reset" tooltipText="Reset" />
-          <SideBarIcon id="card-plus" tooltipText="Create Custom Fields" />
+          <SideBarIcon id="card-plus" tooltipText="Create Custom Card" />
           <SideBarIcon id="card-list" tooltipText="Custom Fields List" />
           <SideBarIcon id="download" tooltipText="Download Cards" />
           <SideBarIcon id="upload" tooltipText="Upload Cards" />
         </div>
       </div>
       <div className="d-flex flex-column justify-content-center align-items-center pb-4">
-        <div>
-          <Icon id="github" size="lg" />
-        </div>
         <a
           href={"https://www.buymeacoffee.com/sauravgupta"}
           target="_blank"
@@ -31,10 +28,18 @@ const Sidebar = () => {
   );
 };
 
-const SideBarIcon = ({ id = "home", tooltipText = "tooltipText" }) => {
+const SideBarIcon = ({
+  id = "home",
+  tooltipText = "tooltipText",
+  selected = false,
+}) => {
   return (
     <Tooltip placement="right" title={tooltipText}>
-      <div className="ft-sidebar--icon p-3 rounded-circle my-4 ft-style-2-shadow-hover">
+      <div
+        className={`ft-sidebar--icon p-3 rounded-circle my-3 ft-style-2-shadow-hover ${
+          selected ? "ft-sidebar--icon--selected" : ""
+        }`}
+      >
         <Icon id={id} />
       </div>
     </Tooltip>
--- src/Components/Layout/header.js
+++ src/Components/Layout/header.js
@@ -1,11 +1,34 @@
 import Icon from "./../UI/Common/Icon/Icon";
+import { Input } from "antd";
 
 const Header = () => {
   return (
-    <div className="fe-header w-100 h-100 px-5">
-      <div>
-        <Icon id="logo" size="" />
+    <div className="fe-header d-flex justify-content-between align-items-center w-100 h-100 px-5">
+      <div className="d-flex align-items-center">
+        <div>
+          <Icon id="logo" size="" />
+        </div>
+        <SearchBar />
       </div>
+      <div>
+        <div className="border rounded-3 cursor-pointer d-flex align-items-center ft-bg-prime98 ft-style-1-shadow ft-border-color-prime88 ft-style-2-shadow-hover  px-3 py-1">
+          <div className="me-2">
+            <Icon id="github" size="md" />
+          </div>
+          <div className="fs-4 fw-bold">Request a feature</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const SearchBar = () => {
+  return (
+    <div className="d-flex align-items-center border-start ms-4 px-4">
+      <div className="ft-icon-dark2">
+        <Icon id="search" size="xs" />
+      </div>
+      <Input placeholder="Search tools ..... " size="large" bordered={false} />
     </div>
   );
 };
