6ff28c73d01f7e927064ba0b6f47212a4ca2f2bb
sauravgupta2800 <sauravgupta2800@gmail.com>
2021-06-25 00:02:13 +0530

    tooltip added

--- src/Components/Layout/Sidebar.js
+++ src/Components/Layout/Sidebar.js
@@ -1,17 +1,18 @@
 import Icon from "../UI/Common/Icon/Icon";
+import { Tooltip } from "antd";
 
 const Sidebar = () => {
   return (
     <div className="ft-sidebar border w-100 h-100 d-flex flex-column">
       <div className="flex-grow-1">
         <div className="d-flex h-100 flex-column justify-content-center align-items-center">
-          <SideBarIcon />
-          <SideBarIcon id="add" />
-          <SideBarIcon id="reset" />
-          <SideBarIcon id="card-plus" />
-          <SideBarIcon id="card-list" />
-          <SideBarIcon id="download" />
-          <SideBarIcon id="upload" />
+          <SideBarIcon id="home" tooltipText="Home" />
+          <SideBarIcon id="add" tooltipText="Add" />
+          <SideBarIcon id="reset" tooltipText="Reset" />
+          <SideBarIcon id="card-plus" tooltipText="Create Custom Fields" />
+          <SideBarIcon id="card-list" tooltipText="Custom Fields List" />
+          <SideBarIcon id="download" tooltipText="Download Cards" />
+          <SideBarIcon id="upload" tooltipText="Upload Cards" />
         </div>
       </div>
       <div className="d-flex flex-column justify-content-center align-items-center pb-4">
@@ -30,11 +31,13 @@ const Sidebar = () => {
   );
 };
 
-const SideBarIcon = ({ id = "home" }) => {
+const SideBarIcon = ({ id = "home", tooltipText = "tooltipText" }) => {
   return (
-    <div className="ft-sidebar--icon p-3 rounded-circle my-4 ft-style-2-shadow-hover">
-      <Icon id={id} />
-    </div>
+    <Tooltip placement="right" title={tooltipText}>
+      <div className="ft-sidebar--icon p-3 rounded-circle my-4 ft-style-2-shadow-hover">
+        <Icon id={id} />
+      </div>
+    </Tooltip>
   );
 };
 
