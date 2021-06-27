2a4fb0baa8d503c82ceb4aa6eb93878f37e32365
sauravgupta2800 <sauravgupta2800@gmail.com>
2021-06-27 14:33:35 +0530

    removed card text added

--- src/Components/Layout/SidebarComponents/AddPopover.js
+++ src/Components/Layout/SidebarComponents/AddPopover.js
@@ -17,7 +17,7 @@ export const AddPopover = (props) => {
   const handleRemoveFromDashboard = (e, { key_name, title }) => {
     e.stopPropagation();
     dispatch(removeCustomCard(key_name));
-    message.success(`${title} card removed from dashboard.`);
+    message.success(`${title} card deleted from list.`);
   };
 
   const text = () => {
@@ -32,30 +32,33 @@ export const AddPopover = (props) => {
             <span className="ms-2">Create Custom Card</span>
           </div>
         </Button>
-        <>
-          {removedList.map((item, index) => (
-            <div
-              key={index}
-              className={`d-flex align-items-center p-2 rounded-3 cursor-pointer add-items-selection ${
-                !index ? "mt-4" : ""
-              }`}
-              onClick={() =>
-                dispatch(setRemovedID({ id: item.key_name, remove: false }))
-              }
-            >
-              <Icon {...item.iconProps} size="lg" />
-              <div className="ms-2 fs-4">{item.title}</div>
-              {addedCustomCards.includes(item.key_name) && (
-                <div
-                  className="ms-2 ft-table-action-icon-red"
-                  onClick={(e) => handleRemoveFromDashboard(e, item)}
-                >
-                  <Icon id="delete" title="Remove from Dashboard" />
-                </div>
-              )}
-            </div>
-          ))}
-        </>
+        {removedList.length ? (
+          <>
+            <div className="mt-4 mb-2 fs-5 ft-color-dark2">REMOVED CARDS </div>
+            {removedList.map((item, index) => (
+              <div
+                key={index}
+                className={`d-flex align-items-center p-2 rounded-3 cursor-pointer add-items-selection`}
+                onClick={() =>
+                  dispatch(setRemovedID({ id: item.key_name, remove: false }))
+                }
+              >
+                <Icon {...item.iconProps} size="lg" />
+                <div className="ms-2 fs-4">{item.title}</div>
+                {addedCustomCards.includes(item.key_name) && (
+                  <div
+                    className="ms-2 ft-table-action-icon-red"
+                    onClick={(e) => handleRemoveFromDashboard(e, item)}
+                  >
+                    <Icon id="delete" title="Delete from list" />
+                  </div>
+                )}
+              </div>
+            ))}
+          </>
+        ) : (
+          ""
+        )}
       </div>
     );
   };
