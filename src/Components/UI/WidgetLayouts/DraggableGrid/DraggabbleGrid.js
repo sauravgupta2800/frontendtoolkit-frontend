a5ec551fcdea2322ce0743bbdd5da33a3f456f95
sauravgupta2800 <sauravgupta2800@gmail.com>
2021-07-03 13:22:25 +0530

    list logic moved

--- src/Components/UI/WidgetLayouts/DraggableGrid/DraggabbleGrid.js
+++ src/Components/UI/WidgetLayouts/DraggableGrid/DraggabbleGrid.js
@@ -1,88 +1,46 @@
 import { Responsive, WidthProvider } from "react-grid-layout";
-import React, { useEffect, useState } from "react";
-// import { getFromLS, isDesktopView, saveToLS } from "../../utils";
-import { getFromLS, isDesktopView, saveToLS } from "../../../utils";
-import { useSelector, useDispatch } from "react-redux";
-import { COMPONENTS } from "../config";
-import { setList } from "../../../../store/widgetsSlice";
-import Icon from "../../Common/Icon/Icon";
+import React, { useState } from "react";
+import { getFromLS, saveToLS } from "../../../utils";
 
 const ResponsiveGridLayout = WidthProvider(Responsive);
 const originalLayouts = getFromLS("layouts") || {};
 
-const DraggableGrid = () => {
-  const dispatch = useDispatch();
-
-  const selectedList = useSelector((state) => {
-    const { list, removedIDs, q } = state.widgets;
-    const filteredList = list.filter(
-      (item) => !removedIDs.includes(item.key_name)
-    );
-    return q.length
-      ? filteredList.filter((item) =>
-          item.title.trim().toLowerCase().includes(q.trim().toLowerCase())
-        )
-      : filteredList;
-  });
-
+const DraggableGrid = ({ selectedList = [] }) => {
   const [layouts, setLayouts] = useState(
     JSON.parse(JSON.stringify(originalLayouts))
   );
-
-  const [show, setShow] = useState(false);
-
-  useEffect(() => {
-    dispatch(setList(COMPONENTS));
-    setShow(true);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
 
   const cols = { lg: 12, md: 8, sm: 6, xs: 4, xxs: 2 };
 
   return (
     <div className="ft-draggable-grid d-flex w-100 h-100 overflow-auto">
-      {show && (
-        <>
-          {selectedList.length ? (
-            <ResponsiveGridLayout
-              layouts={layouts}
-              cols={cols}
-              onLayoutChange={(layout, layouts) => {
-                saveToLS("layouts", layouts);
-                setLayouts(layouts);
-              }}
-              rowHeight={30}
-              containerPadding={[20, 20]}
-              margin={[15, 15]}
-              style={{ minWidth: "100%", paddingBottom: "5rem" }}
-              isBounded={true}
-              draggableHandle=".draggableClassName"
-            >
-              {selectedList.map((item) => (
-                <div
-                  key={item.key_name}
-                  data-grid={item.dataGrid}
-                  className="ft-style-1-shadow ft-style-2-shadow-hover"
-                >
-                  {React.createElement(item.component, {
-                    key: item.key_name,
-                    ...item,
-                  })}
-                </div>
-              ))}
-            </ResponsiveGridLayout>
-          ) : (
-            <div className="w-100 h-100 d-flex flex-column align-items-center justify-content-center">
-              <Icon
-                showCursor={false}
-                id="empty"
-                size={isDesktopView ? "" : "xxxl"}
-              />
-              <div className="fs-1 mt-4">No card available</div>
-            </div>
-          )}
-        </>
-      )}
+      <ResponsiveGridLayout
+        layouts={layouts}
+        cols={cols}
+        onLayoutChange={(layout, layouts) => {
+          saveToLS("layouts", layouts);
+          setLayouts(layouts);
+        }}
+        rowHeight={30}
+        containerPadding={[20, 20]}
+        margin={[15, 15]}
+        style={{ minWidth: "100%", paddingBottom: "5rem" }}
+        isBounded={true}
+        draggableHandle=".draggableClassName"
+      >
+        {selectedList.map((item) => (
+          <div
+            key={item.key_name}
+            data-grid={item.dataGrid}
+            className="ft-style-1-shadow ft-style-2-shadow-hover"
+          >
+            {React.createElement(item.component, {
+              key: item.key_name,
+              ...item,
+            })}
+          </div>
+        ))}
+      </ResponsiveGridLayout>
     </div>
   );
 };
--- src/Components/UI/WidgetLayouts/WidgetLayouts.js
+++ src/Components/UI/WidgetLayouts/WidgetLayouts.js
@@ -1,11 +1,49 @@
 import DraggableGrid from "./DraggableGrid/DraggabbleGrid";
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import Icon from "../Common/Icon/Icon";
+import { setList } from "../../../store/widgetsSlice";
+import { COMPONENTS } from "./config";
 
 const WidgetLayouts = () => {
+  const dispatch = useDispatch();
+
+  const [show, setShow] = useState(false);
+
+  const selectedList = useSelector((state) => {
+    const { list, removedIDs, q } = state.widgets;
+    const filteredList = list.filter(
+      (item) => !removedIDs.includes(item.key_name)
+    );
+    return q.length
+      ? filteredList.filter((item) =>
+          item.title.trim().toLowerCase().includes(q.trim().toLowerCase())
+        )
+      : filteredList;
+  });
+
+  useEffect(() => {
+    dispatch(setList(COMPONENTS));
+    setShow(true);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div className="widget-layouts w-100 h-100">
-      <div className="widget-layouts--header">header</div>
+      <div className="widget-layouts--header ft-style-1-shadow">header</div>
       <div className="widget-layouts--content ft-bg-prime97">
-        <DraggableGrid />
+        {show && (
+          <>
+            {selectedList.length ? (
+              <DraggableGrid selectedList={selectedList} />
+            ) : (
+              <div className="w-100 h-100 d-flex flex-column align-items-center justify-content-center">
+                <Icon showCursor={false} id="empty" size="xxxl" />
+                <div className="fs-1 mt-4">No card available</div>
+              </div>
+            )}
+          </>
+        )}
       </div>
     </div>
   );
