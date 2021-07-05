0867bfcb993e06803605ca432d787aae0b9f277d
sauravgupta2800 <sauravgupta2800@gmail.com>
2021-07-05 23:45:32 +0530

    layout modified

--- src/Components/UI/FELayout/FELayout.js
+++ src/Components/UI/FELayout/FELayout.js
@@ -3,20 +3,55 @@ import {
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
+import MainLayout from "../../Layout/MainLayout";
 import Icon from "../Common/Icon/Icon";
 import { ROADMAP_MENU } from "./config";
 
 const FELayout = () => {
   return (
-    <div className="ft-vertical-timeline w-100 h-100 overflow-auto">
-      <VerticalTimeline>
-        {ROADMAP_MENU.map((menu) => (
+    <div className="ft-vertical-timeline w-100 h-100">
+      <div className="ft-vertical-timeline--header ft-style-1-shadow ft-bg-light100">
+        hello
+      </div>
+      <div className="ft-vertical-timeline--content ft-bg-prime93 overflow-auto">
+        <VerticalTimeline>
+          {ROADMAP_MENU.map((menu) => (
+            <VerticalTimelineElement
+              textClassName="ft-vertical-timeline--content-element--card"
+              contentStyle={{
+                border: `1px solid ${menu.darkColor}`,
+              }}
+              contentArrowStyle={{
+                borderRight: `10px solid ${menu.darkColor}`,
+              }}
+              iconStyle={menu.iconStyle}
+              iconClassName=""
+              icon={<Icon id={menu.key} size={menu.iconSize || "md"} />}
+            >
+              <div
+                className="ft-color-primeshade45 fs-3 fw-bold"
+                style={{ color: menu.darkColor }}
+              >
+                {menu.title}
+              </div>
+              <div className="fs-4 ft-color-dark2">{menu.description}</div>
+              <div className="w-100 d-flex justify-content-center">
+                <div
+                  className="py-2 px-4 cursor-pointer fs-5 rounded-6"
+                  style={{ background: menu.darkColor, color: "#fff" }}
+                >
+                  See Details
+                </div>
+              </div>
+            </VerticalTimelineElement>
+          ))}
           <VerticalTimelineElement
-            className="ft-vertical-timeline-element--card"
+            className="vertical-timeline-element--work"
             contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-            contentArrowStyle={{ borderRight: "8px solid  rgb(33, 150, 243)" }}
-            iconStyle={menu.iconStyle}
-            icon={<Icon id={menu.key} size={menu.iconSize || "md"} />}
+            contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
+            date="2011 - present"
+            iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+            icon={<Icon id="table" />}
           >
             <h3 className="vertical-timeline-element-title">
               Creative Director
@@ -27,23 +62,8 @@ const FELayout = () => {
               Management, Team Leading
             </p>
           </VerticalTimelineElement>
-        ))}
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-          contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
-          date="2011 - present"
-          iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-          icon={<Icon id="table" />}
-        >
-          <h3 className="vertical-timeline-element-title">Creative Director</h3>
-          <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
-          <p>
-            Creative Direction, User Experience, Visual Design, Project
-            Management, Team Leading
-          </p>
-        </VerticalTimelineElement>
-      </VerticalTimeline>
+        </VerticalTimeline>
+      </div>
     </div>
   );
 };
--- src/Components/UI/FELayout/config.js
+++ src/Components/UI/FELayout/config.js
@@ -1,61 +1,94 @@
 export const ROADMAP_MENU = [
   {
     key: "command",
+    darkColor: "#59585d",
     title: "Command Line",
+    description:
+      "The terminal is an interface used to execute text commands,and it gives you access to the underlying operating system. Basic terminal usage is a skill all developers need regardless of their specialization. Command line is very important so I strongly recommend you study more on how to use it.",
     iconStyle: { background: "#59585d", color: "#fff" },
   },
   {
     key: "git",
+    darkColor: "#eb636d",
     title: "Git - version control",
+    description:
+      "Git is a version control system that enables developers to track changes in their project. Git also helps developers collaborate as a team. Git is needed among developers, to ensure that there are no code conflicts between developers.",
     iconStyle: { background: "#eb636d", color: "white" },
   },
   {
     key: "github",
+    darkColor: "#767c84",
     title: "Github",
+    description:
+      "GitHub is a code hosting platform for software development. GitHub lets teams work together on projects and it is also used for version control. ",
     iconStyle: { background: "#767c84", color: "white" },
   },
   {
     key: "vs-code",
-    title: "IDE",
+    darkColor: "#69a9fd",
+    title: "IDE - Visual Studio Code",
+    description:
+      "visual studio code is the best IDE for front-end development. Visual studio code comes with a great deal of extension which helps in improving productivity as a front-end developer.",
     iconStyle: { background: "#d7eeff", color: "white" },
   },
   {
     key: "web",
+    darkColor: "#8068f8",
     title: "Web fundamentals",
+    description:
+      "It is important to understand how the web works before you start learning any technologies. You should learn about things like: Internet, HTTP, HTTPS, Browsers etc",
     iconStyle: { background: "#8068f8", color: "white" },
   },
   {
     key: "html",
+    darkColor: "#fc8436",
     title: "HTML",
     iconSize: "lg",
+    description:
+      "HTML stands for Hypertext Markup Language. It is the markup language for building web pages, it is also the building block of the web. HTML is easy to learn and comprehend. With just HTML, you can build a basic website.",
     iconStyle: { background: "#fad4aa", color: "white" },
   },
   {
     key: "css3",
+    darkColor: "#09a2f1",
     title: "CSS",
     iconSize: "lg",
+    description:
+      "CSS stands for Cascading Style Sheets. It is the technology to learn after HTML. It is used for styling our HTML. For example, we can use CSS to space our content, colours, fonts, etc.",
     iconStyle: { background: "#a5dbf7", color: "white" },
   },
   {
     key: "javascript",
+    darkColor: "#f9c523",
     title: "Javascript",
     iconSize: "lg",
+    description:
+      "JavaScript is one of the most popular programming languages in the world. It is the language of the web. As a front-end developer, it is required you learn JavaScript. JavaScript enables us to create dynamic content. When you create your HTML structure and your style with your CSS, JavaScript makes the website dynamic and alive.",
     iconStyle: { background: "#f9fcd2", color: "white" },
   },
   {
     key: "react",
+    darkColor: "#68befc",
     title: "React",
     iconSize: "xl",
+    description:
+      " a JavaScript library for building user interfaces React lets you create reusable components.",
     iconStyle: { background: "#68befc", color: "white" },
   },
   {
     key: "vue",
+    darkColor: "#61c398",
     title: "Vue",
+    description:
+      " Vuejs is an approachable, versatile, performant javascript framework that helps you create a maintainable and testable codebase. Vuejs is also beginner-friendly.",
     iconStyle: { background: "#c1fae1", color: "white" },
   },
   {
     key: "deployment",
-    title: "deployment",
+    darkColor: "#e956f5",
+    title: "Deployment",
+    description:
+      "With web deployments, you can host your simple application on the internet. It is super easy to do. You can use tools like GitHub pages, or Netlify.",
     iconStyle: { background: "#e956f5", color: "white" },
   },
 ];
