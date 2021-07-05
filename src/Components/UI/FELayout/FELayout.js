9eb3b6194fcb637145939014d0d24906aa760ba3
sauravgupta2800 <sauravgupta2800@gmail.com>
2021-07-05 17:56:18 +0530

    react-vertical-timeline-component created

--- src/Components/UI/FELayout/FELayout.js
+++ src/Components/UI/FELayout/FELayout.js
@@ -1,5 +1,51 @@
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import Icon from "../Common/Icon/Icon";
+import { ROADMAP_MENU } from "./config";
+
 const FELayout = () => {
-  return "FELayout";
+  return (
+    <div className="ft-vertical-timeline w-100 h-100 overflow-auto">
+      <VerticalTimeline>
+        {ROADMAP_MENU.map((menu) => (
+          <VerticalTimelineElement
+            className="ft-vertical-timeline-element--card"
+            contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+            contentArrowStyle={{ borderRight: "8px solid  rgb(33, 150, 243)" }}
+            iconStyle={menu.iconStyle}
+            icon={<Icon id={menu.key} size={menu.iconSize || "md"} />}
+          >
+            <h3 className="vertical-timeline-element-title">
+              Creative Director
+            </h3>
+            <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+            <p>
+              Creative Direction, User Experience, Visual Design, Project
+              Management, Team Leading
+            </p>
+          </VerticalTimelineElement>
+        ))}
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
+          date="2011 - present"
+          iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+          icon={<Icon id="table" />}
+        >
+          <h3 className="vertical-timeline-element-title">Creative Director</h3>
+          <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+          <p>
+            Creative Direction, User Experience, Visual Design, Project
+            Management, Team Leading
+          </p>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+    </div>
+  );
 };
 
 export default FELayout;
---
+++ src/Components/UI/FELayout/config.js
@@ -0,0 +1,61 @@
+export const ROADMAP_MENU = [
+  {
+    key: "command",
+    title: "Command Line",
+    iconStyle: { background: "#59585d", color: "#fff" },
+  },
+  {
+    key: "git",
+    title: "Git - version control",
+    iconStyle: { background: "#eb636d", color: "white" },
+  },
+  {
+    key: "github",
+    title: "Github",
+    iconStyle: { background: "#767c84", color: "white" },
+  },
+  {
+    key: "vs-code",
+    title: "IDE",
+    iconStyle: { background: "#d7eeff", color: "white" },
+  },
+  {
+    key: "web",
+    title: "Web fundamentals",
+    iconStyle: { background: "#8068f8", color: "white" },
+  },
+  {
+    key: "html",
+    title: "HTML",
+    iconSize: "lg",
+    iconStyle: { background: "#fad4aa", color: "white" },
+  },
+  {
+    key: "css3",
+    title: "CSS",
+    iconSize: "lg",
+    iconStyle: { background: "#a5dbf7", color: "white" },
+  },
+  {
+    key: "javascript",
+    title: "Javascript",
+    iconSize: "lg",
+    iconStyle: { background: "#f9fcd2", color: "white" },
+  },
+  {
+    key: "react",
+    title: "React",
+    iconSize: "xl",
+    iconStyle: { background: "#68befc", color: "white" },
+  },
+  {
+    key: "vue",
+    title: "Vue",
+    iconStyle: { background: "#c1fae1", color: "white" },
+  },
+  {
+    key: "deployment",
+    title: "deployment",
+    iconStyle: { background: "#e956f5", color: "white" },
+  },
+];
